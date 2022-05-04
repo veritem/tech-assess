4179e5d7a6cc95dd8f1b578b3fd7faa335d480ad
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-05 00:21:23 +0200

    feat: added chart

---
+++ src/components/Table.tsx
@@ -0,0 +1,92 @@
+import React, { Fragment, useMemo } from 'react'
+import { useSortBy, useTable } from 'react-table'
+import { Todo } from '../features/todo-slice'
+
+export default function Table({ data }: { data: Todo[] }) {
+
+    console.log({ data })
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'ID',
+                accessor: 'id'
+            },
+            {
+                Header: 'Title',
+                accessor: 'title'
+            },
+            {
+                Header: 'completed',
+                accessor: 'completed',
+            }
+        ],
+        []
+    ) as any
+
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+        { columns, data },
+        useSortBy
+    )
+
+
+    return (
+        <Fragment>
+            <table className="text-center table-auto" {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr
+                            {...headerGroup.getHeaderGroupProps()}
+                            className="bg-primary text-blue text-sm py-4 px-5"
+                        >
+                            {headerGroup.headers.map((column) => (
+                                <th
+                                    {...column.getHeaderProps(
+                                        column.getSortByToggleProps()
+                                    )}
+                                    className="py-4 px-2"
+                                >
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? '↓'
+                                                : '↑'
+                                            : ''}
+                                    </span>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, index) => {
+                        prepareRow(row)
+                        return (
+                            <tr
+                                {...row.getRowProps()}
+                                className={`cursor-pointer py-4 hover:bg-hover ${index % 2 !== 0 ? 'bg-alternateTable' : 'bg-white'
+                                    }`}
+                            >
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()} className="px-7 py-2">
+                                            {' '}
+                                            {
+                                                cell.render('Cell')
+                                            }{' '}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+        </Fragment>
+
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,32 @@
+import Table from "../components/Table";
 import { useFetchTodosQuery } from "../features/todo-slice";
 
 export default function Home() {
 
     const { data, isLoading, error } = useFetchTodosQuery()
 
+
+
     if (isLoading) {
         return <div>Loading...</div>
     }
-
 
     if (error) {
         return <div>Error: {JSON.stringify(error, null, 2)} </div>
     }
 
+    if (!data) {
+        return <div>No Data</div>
+    }
+
+
+
+
 
     return (
         <section>
             <div className="px-20 py-5">
-                <h2>Home page</h2>
+                <Table data={data} />
             </div>
         </section>
     );
